7456d7ce681ded038fd45ce37b328bf15eebd311
Aldo Xhafaj <Aldo.Xhafaj@raiffeisen.al>
2025-06-10 15:07:23 +0200

    feature/quiz-00 vite config setup and tailwind integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@quizstream/pages',
+        replacement: resolve(__dirname, './src/pages'),
+      },
+    ],
+  },
 });
