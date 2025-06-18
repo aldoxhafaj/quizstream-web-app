e51205d736a2044163c82557ccee86ef474270b9
Aldo Xhafaj <Aldo.Xhafaj@raiffeisen.al>
2025-06-18 16:12:17 +0200

    feature/quiz-08 created checkbox component

---
+++ src/components/checkbox/types.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-restricted-imports
+import { type CheckboxProps as HeroUICheckboxProps } from '@heroui/checkbox';
+
+export type CheckboxProps = Pick<
+  HeroUICheckboxProps,
+  | 'children'
+  | 'classNames'
+  | 'icon'
+  | 'value'
+  | 'name'
+  | 'size'
+  | 'color'
+  | 'radius'
+  | 'lineThrough'
+  | 'isSelected'
+  | 'defaultSelected'
+  | 'isRequired'
+  | 'isReadOnly'
+  | 'isDisabled'
+  | 'isIndeterminate'
+  | 'isInvalid'
+  | 'onValueChange'
+>;
