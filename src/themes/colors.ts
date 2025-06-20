af1be53ee724f5809cb521ca7374b2db8f9e0a8d
Aldo Xhafaj <Aldo.Xhafaj@raiffeisen.al>
2025-06-20 11:52:59 +0200

    feature/quiz-10-part-3 added theme colors for content loaders and loading ability to lottie animation

---
+++ src/themes/colors.ts
@@ -0,0 +1,18 @@
+/** These colors must be used only in unavoidable cases. You will be able
+ * to use them through useTheme custom hook so you apply them based on theme.
+ * Otherwise use tailwind classes.
+ *
+ * Rule ***
+ * You should add here only colors that you use in critical cases. Also all the colors
+ * that are here should be included in tailwind as well but not vice versa.
+ */
+
+export enum DarkColors {
+  CHARCOAL = '#23262f',
+  DUSK = '#35383f',
+}
+
+export enum LightColors {
+  CHARCOAL = '#e1dfdf',
+  DUSK = '#f3f0f0',
+}
