f1c5868c38e2bf45767111ef52e65c682d4d7ee4
Aldo Xhafaj <Aldo.Xhafaj@raiffeisen.al>
2025-06-26 09:28:31 +0200

    feature/quiz-13 created carousel story

---
+++ src/components/carousel/Carousel.stories.ts
@@ -0,0 +1,54 @@
+import { Animation } from '@quizstream/themes/animations';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { fn } from 'storybook/test';
+
+import { Carousel } from './Carousel';
+import type { Slide } from './types';
+
+const slides: Slide[] = [
+  {
+    id: 1,
+    title: 'First slide',
+    animation: { name: Animation.QUESTION_MARK_TRIPLON, size: 250 },
+  },
+  {
+    id: 2,
+    title: 'Second slide',
+    animation: { name: Animation.QUESTION_MARK, size: 250 },
+  },
+  {
+    id: 3,
+    title: 'Third slide',
+    animation: { name: Animation.BRAIN_WAVE, size: 250 },
+  },
+];
+
+const meta = {
+  title: 'Carousel',
+  component: Carousel,
+  tags: ['autodocs'],
+  parameters: {
+    enableTheme: true,
+  },
+
+  argTypes: {
+    duration: {
+      options: [1000, 3000, 5000, 10000],
+      control: { type: 'radio' },
+    },
+  },
+
+  args: { onSlidePress: fn() },
+} satisfies Meta<typeof Carousel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const CarouselStory: Story = {
+  args: {
+    slides,
+    duration: 3000,
+    autoplay: true,
+    loop: false,
+  },
+};
